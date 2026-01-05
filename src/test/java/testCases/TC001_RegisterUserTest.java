11be09c228338893b2de2a52b986c1d548ea3356
TheRuchika <ruchikapromodya@gmail.com>
2026-01-05 13:07:34 +0530

    Initial commit - WebPage Automation POM

---
+++ src/test/java/testCases/TC001_RegisterUserTest.java
@@ -0,0 +1,36 @@
+package testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+import java.time.Duration;
+
+public class TC001_RegisterUserTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openPage(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demo.guru99.com/test/newtours/index.php");
+    }
+
+    @Test
+    public void TC001(){
+
+        HomePage homePage = new HomePage(driver);
+
+        homePage.clickRegisterMenu();
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+
+    }
+}
